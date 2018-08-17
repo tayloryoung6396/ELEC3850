fc55998a1dd60260f230372b78711287171bc14e
Unknown <alexrendinaaa@gmail.com>
2018-08-17 22:37:31 +1000

    hey alex pushed something- dont trust it

---
+++ Project/modules/input/src/updatevalues.h
@@ -0,0 +1,17 @@
+#ifndef UPDATEVALUES_H_INCLUDED
+#define UPDATEVALUES_H_INCLUDED
+
+#pragma once
+
+#include "joystick.h"
+#include <iostream>
+#include <climits>
+
+void update_buttons(Joystick* joy);
+void update_axes(Joystick* joy);
+
+short bound_short(int num);
+short map(long x, long in_min, long in_max, long out_min, long out_max);
+short short_map(long x);
+
+#endif // UPDATEVALUES_H_INCLUDED
