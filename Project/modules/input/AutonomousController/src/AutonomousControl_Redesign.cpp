feeab5c2644cd8f5422a5b100bd008faeaaf35d8
Unknown <c3206230@uon.edu.au>
2018-09-06 22:32:30 +1000

    Autonomous Controller Redesign

--- Project/modules/input/AutonomousController/src/AutonomousControl_Redesign.cpp
+++ Project/modules/input/AutonomousController/src/AutonomousControl_Redesign.cpp
@@ -29,21 +29,86 @@ Simple_Path_Planner() {
     // This will be a vector of pairs X Y in tank space
 }
 
-Recalculate_path() {}
+Recalculate_path() {
+	// So i didn't end up where i wanted to, 
+	// but i should be able to take my position and the goal and recalculate a new vector
+}
 
 /********************************Motor_Controller********************************/
 
 Motor_Controller(){
 	// Iterate through each vector of pairs
-	// Convert it to a polar vector and execute it
+	// Convert it to a polar vector and execute it - all in tank space
 	Motor_Driver(Polar_Vector);
 }
 
 Motor_Driver(Forward, Rotation){
 	// Take the forward and roation relative to the tank and convert it into a pair of motor commands
-	// This will be a revolution amount/position
+	// This will be a revolution amount/position - this might just be a total amount irrelevant of revolutions and have it accounted for later
+
+	// Set tank to moving[2] if we get a non zero input
 }
 
 Motor_Director(){
-	
+	// Remembering that this has to happen over both motors, not sure how this will affect the current plan
+
+	// Essentially if either controller is running, present position and previous position to figure out the distance step
+
+	// We might only want to look at this when we are moving, if we aren't we dont really care
+	if(moving){
+	    // update the localisation position either way to the current position
+	    // This is done by taking the last localisation position (which might have been updated along the path) 
+	    // as well as the last motor position that is linked with that same localisation position
+	    // and taking the difference between the current and prev motor positions and reverse engineering the vector position
+	    
+	    // this needs to keep track of the revolutions the motor has done
+	    // ALTERNATIVELY
+		// use just a total distance and subtract some distance travelled accounting for the overflowing
+
+	    if(Autonomour_Controller){
+			// Check whether we have completed the vector yet 		
+			if(complete){
+				// If that specific vector has been "completed" check to see if the executed final position matches the goal revolution/position
+				// If it does no need to update any of the other vectors continue but still update the localisation position
+				// If it doesn't then we need to adjust our list of vectors to put us back on path.
+			}
+		}
+	}
 }
+
+/********************************Autonomous_Controller********************************/
+
+// This will be a state machine that runs based on what case we are in
+
+
+if(!have object){
+    if(known object pos){
+        // go to the object
+        path planner
+    }
+    else if(unknown object pos){
+        // search for it
+        weighted_search()
+    }
+}
+
+else if (have object){
+    if(known goal pos){
+        // go to the goal
+        path planner
+    }
+    else if(unknown goal pos){
+        // search for it
+        weighted_search()
+    }
+}
+
+/********************************PS3_Controller********************************/
+
+// Here we read in buttons and what not.
+// The joystick is some scaled velocity value
+// Maybe between some min velocity and max velocity
+
+// This then directly set the motor speed
+// ALTERNATIVELY
+// Use the values to set Motor_Driver(Polar_Vector); - tank space
