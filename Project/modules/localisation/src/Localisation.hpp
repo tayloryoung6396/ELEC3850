5a0430c016c22be063c6af4610aec82777c40cf3
ALEXBR <alexrendinaaa@gmail.com>
2018-09-24 14:55:41 +1000

    localisation, up to finding an appropriate function

--- Project/modules/localisation/src/Localisation.hpp
+++ Project/modules/localisation/src/Localisation.hpp
@@ -8,9 +8,11 @@ extern int Localisation_main();
 
 class Localistation {
 public:
-    static double w_Tank_Position[2];
-    static double w_Tank_Rotation;
-    static double w_Goal_Position[2];
+    static double w_Tank_Position[2];   //where i am in world, read in as {x,y} where x is forward and y si to the left.
+    static double w_Tank_Rotation;      //angle in radians counterclockwise
+    static double w_Goal_Position[2];   //wrt front of tank and the world
+    static double gens_sonics[SENSORS]; //this variable will need to be names and connected up with gens sonics
+
 };
 
 #endif /* LOCALISATION_H_ */
