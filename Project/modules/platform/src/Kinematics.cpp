9c90f98d07caca389fa1581d7f831688c76d8b13
Unknown <c3206230@uon.edu.au>
2018-09-27 18:25:33 +1000

    Remove yaw restriction

--- Project/modules/platform/src/Kinematics.cpp
+++ Project/modules/platform/src/Kinematics.cpp
@@ -24,8 +24,8 @@ double Kinematics::tank_length      = 0.355;
 double Kinematics::tank_width       = 0.180;
 double Kinematics::tank_height      = 0.150;
 
-double Kinematics::Servo_max_limit[5] = {0, M_PI / 2.0, M_PI / 2.0, M_PI / 2.0, M_PI / 4.0};
-double Kinematics::Servo_min_limit[5] = {0, -M_PI / 2.0, -M_PI / 2.0, -M_PI / 2.0, -M_PI / 4.0};
+double Kinematics::Servo_max_limit[5] = {M_PI, M_PI / 2.0, M_PI / 2.0, M_PI / 2.0, M_PI / 4.0};
+double Kinematics::Servo_min_limit[5] = {-M_PI, -M_PI / 2.0, -M_PI / 2.0, -M_PI / 2.0, -M_PI / 4.0};
 double Kinematics::Servo_offset[5]    = {0, 0, 0, 0, 0};
 
 void Kinematics_init() {
