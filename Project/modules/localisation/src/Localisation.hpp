7cb1bedb37f2c52ab2f468efdeaec96811d4bc26
Unknown <c3206230@uon.edu.au>
2018-09-27 11:27:53 +1000

    .

--- Project/modules/localisation/src/Localisation.hpp
+++ Project/modules/localisation/src/Localisation.hpp
@@ -6,13 +6,13 @@
 extern void Localisation_init();
 extern int Localisation_main();
 
-class Localistation {
-public:
-    static double w_Tank_Position[2];   //where i am in world, read in as {x,y} where x is forward and y si to the left.
-    static double w_Tank_Rotation;      //angle in radians counterclockwise
-    static double w_Goal_Position[2];   //wrt front of tank and the world
-    static double gens_sonics[SENSORS]; //this variable will need to be names and connected up with gens sonics
+// class Localistation {
+// public:
+//     static double w_Tank_Position[2];   //where i am in world, read in as {x,y} where x is forward and y si to the
+//     left. static double w_Tank_Rotation;      //angle in radians counterclockwise static double w_Goal_Position[2];
+//     //wrt front of tank and the world static double gens_sonics[SENSORS]; //this variable will need to be names and
+//     connected up with gens sonics
 
-};
+// };
 
 #endif /* LOCALISATION_H_ */
