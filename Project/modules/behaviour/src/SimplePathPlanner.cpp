2d199a54bf8e10bb8d33873909b7f1d6052e771d
Unknown <c3206230@uon.edu.au>
2018-09-29 10:45:58 +1000

    Autonomous Control Branch

--- Project/modules/behaviour/src/SimplePathPlanner.cpp
+++ Project/modules/behaviour/src/SimplePathPlanner.cpp
@@ -15,18 +15,18 @@ int SimplePathPlanner() {
 
     // Calculate path from w_Tank_Position to w_Goal_Position
     // This will be a vector of pairs X Y in tank space
-    // double t_Goal_Position[2] = {Localisation::w_Goal_Position[0] - Localistation::w_Tank_Position[0],
-    //                              Localisation::w_Goal_Position[1] - Localistation::w_Tank_Position[1]};
-    // // Calculate the rotation difference
-    // double t_Goal_Rotation = std::atan2(t_Goal_Position[1], t_Goal_Position[0]);
+    double t_Goal_Position[2] = {Localisation::w_Goal_Position[0] - Localistation::w_Tank_Position[0],
+                                 Localisation::w_Goal_Position[1] - Localistation::w_Tank_Position[1]};
+    // Calculate the rotation difference
+    double t_Goal_Rotation = std::atan2(t_Goal_Position[1], t_Goal_Position[0]);
 
-    // // Convert into a forward and rotation command
-    // double Forward  = std::sqrt(std::pow(t_Goal_Position[0], 2) + std::pow(t_Goal_Position[1], 2));
-    // double Rotation = Localistation::w_Tank_Rotation - t_Goal_Rotation;
-    // PathPlanner pplanner;
+    // Convert into a forward and rotation command
+    double Forward  = std::sqrt(std::pow(t_Goal_Position[0], 2) + std::pow(t_Goal_Position[1], 2));
+    double Rotation = Localistation::w_Tank_Rotation - t_Goal_Rotation;
+    PathPlanner pplanner;
 
-    // // Rotation is relative to the vehicle, not the world
-    // pplanner.emplace_path(Forward, Rotation);
+    // Rotation is relative to the vehicle, not the world
+    pplanner.emplace_path(Forward, Rotation);
     return 0;
 }
 
