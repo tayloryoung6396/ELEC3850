354212a9f72c159d563ca6a0d22c3b2917f14d88
Unknown <c3206230@uon.edu.au>
2018-09-29 10:14:57 +1000

    Multi sensor test intergrated

--- Project/modules/vision/UltrasonicSensor/src/UltrasonicSensor.cpp
+++ Project/modules/vision/UltrasonicSensor/src/UltrasonicSensor.cpp
@@ -10,6 +10,28 @@ uint32_t Ultrasonic::Start_time                 = 0;
 uint32_t Ultrasonic::sensor_return[SENSORS]     = {0};
 double Ultrasonic::Detection_distances[SENSORS] = {0};
 
+// Interrupts
+void myInterrupt0(void) {  // INTERRUPT FOR SENSOR 1
+    std::cout << "INT 1" << std::endl;
+    Ultrasonic::sensor_return[0] = micros();
+}
+
+void myInterrupt1(void) {  // INTERRUPT FOR SENSOR 2
+    std::cout << "INT 2" << std::endl;
+    Ultrasonic::sensor_return[1] = micros();
+}
+
+void myInterrupt2(void) {  // INTERRUPT FOR SENSOR 3
+    std::cout << "INT 3" << std::endl;
+    Ultrasonic::sensor_return[2] = micros();
+}
+
+void myInterrupt3(void) {  // INTERRUPT FOR SENSOR 4
+    std::cout << "INT 4" << std::endl;
+    Ultrasonic::sensor_return[3] = micros();
+}
+
+
 void UltrasonicSensor_init() {
     std::cout << "Initilising ULTRASONIC SENSOR" << std::endl;
     // Here we setup the gpio pins
@@ -22,6 +44,20 @@ void UltrasonicSensor_init() {
     pinMode(ECHO1, INPUT);  // ECH0 IS AN OUTPUT FOR RPI TO SENSOR 2
     pinMode(ECHO2, INPUT);  // ECH0 IS AN OUTPUT FOR RPI TO SENSOR 3
     pinMode(ECHO3, INPUT);  // ECH0 IS AN OUTPUT FOR RPI TO SENSOR 4
+
+    // SET UP INTERRUPTS
+    if (wiringPiISR(ECHO0, INT_EDGE_FALLING, &myInterrupt0) < 0) {
+        std::cout << "Unable to setup ISR 0: " << strerror(errno) << std::endl;
+    }
+    else if (wiringPiISR(ECHO1, INT_EDGE_FALLING, &myInterrupt1) < 0) {
+        std::cout << "Unable to setup ISR 1: " << strerror(errno) << std::endl;
+    }
+    else if (wiringPiISR(ECHO2, INT_EDGE_FALLING, &myInterrupt2) < 0) {
+        std::cout << "Unable to setup ISR 2: " << strerror(errno) << std::endl;
+    }
+    else if (wiringPiISR(ECHO3, INT_EDGE_FALLING, &myInterrupt3) < 0) {
+        std::cout << "Unable to setup ISR 3: " << strerror(errno) << std::endl;
+    }
 
     digitalWrite(TRIG, LOW);  // TRIGGER PIN MUST START LOW
     delayMicroseconds(60);    // DELAY TO ALLOW PINS TO SET
@@ -39,45 +75,43 @@ int UltrasonicSensor_main() {
     DistanceM();  // DISTANCE DETECTION
 
     // DEBUG DISTANCE CALCULATIONS
-    std::cout << "Right Sensor Distance " << Ultrasonic::Detection_distances[0] << " m" << std::endl;
-    // std::cout << "Back Sensor Distance " << Ultrasonic::Detection_distances[1] << " m" << std::endl;
-    // std::cout << "Left Sensor Distance " << Ultrasonic::Detection_distances[2] << " m" << std::endl;
+    std::cout << "Sensor 0 Distance " << Ultrasonic::Detection_distances[0] << " m" << std::endl;
+    std::cout << "Sensor 1 Distance " << Ultrasonic::Detection_distances[1] << " m" << std::endl;
+    std::cout << "Sensor 2 Distance " << Ultrasonic::Detection_distances[2] << " m" << std::endl;
+    std::cout << "Sensor 3 Distance " << Ultrasonic::Detection_distances[3] << " m" << std::endl;
     return 0;
 }
 
 void Sendpulse() {
-    int echo_pin[SENSORS] = {ECHO1};
-    int trig_pin[SENSORS] = {TRIG};
-    for (int sensor = 0; sensor < SENSORS; sensor++) {
-        digitalWrite(trig_pin[sensor], HIGH);
-        delayMicroseconds(100);  // 10us Delay
-        digitalWrite(trig_pin[sensor], LOW);
+    // Reset all of the times
+    Ultrasonic::sensor_return[0] = -1;
+    Ultrasonic::sensor_return[1] = -1;
+    Ultrasonic::sensor_return[2] = -1;
+    Ultrasonic::sensor_return[3] = -1;
 
-        // WAIT FOR ECHO START
-        delayMicroseconds(400);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(100);  // 10us Delay
+    digitalWrite(TRIG, LOW);
 
-        Ultrasonic::Start_time = micros();
-        while (1) {
-            if (micros() - Ultrasonic::Start_time > TIMEOUT) {
-                std::cout << "TIMEOUT" << std::endl;
-                break;
-            }
-            else if (digitalRead(echo_pin[sensor]) == HIGH) {
-                Ultrasonic::sensor_return[sensor] = micros() - Ultrasonic::Start_time;
-                break;
-            }
-        }
+    // WAIT FOR ECHO START
+    delayMicroseconds(400);
 
-        // std::cout << "Start time " << Ultrasonic::Start_time << " Return time " << Ultrasonic::sensor_return[sensor]
-        //           << std::endl;
+    Ultrasonic::Start_time = micros();
+
+    while ((Ultrasonic::sensor_return[0] == -1 || Ultrasonic::sensor_return[1] == -1
+            || Ultrasonic::sensor_return[2] == -1 || Ultrasonic::sensor_return[3] == -1)
+           && ((micros() - Ultrasonic::Start_time < TIMEOUT))) {
     }
 }
 
 void DistanceM() {
     for (int sensor = 0; sensor < SENSORS; sensor++) {
-        Ultrasonic::Detection_distances[sensor] =
-            Ultrasonic::sensor_return[sensor] * 0.00017
-            + Kinematics::ultrasonic_offset[sensor];  // DISTANCE CALCULATION IN METRES
+        // Only calculate it if the distance was not a timeout
+        if (Ultrasonic::sensor_return[sensor] != -1) {
+            Ultrasonic::Detection_distances[sensor] =
+                (Ultrasonic::sensor_return[sensor] - Ultrasonic::Start_time) * 0.00017;
+            // + Kinematics::ultrasonic_offset[sensor];  // DISTANCE CALCULATION IN METRES
+        }
     }
 }
 
