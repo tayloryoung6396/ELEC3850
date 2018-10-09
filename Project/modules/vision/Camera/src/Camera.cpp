9aba2436da5074ef906d06feee383a5790649e2e
Unknown <genevieve-24@hotmail.com>
2018-10-09 12:18:39 +1100

    Array Red Split Test 1

--- Project/modules/vision/Camera/src/Camera.cpp
+++ Project/modules/vision/Camera/src/Camera.cpp
@@ -33,59 +33,74 @@ void Camera_init() {
 }
 
 int Camera_main() {
-    // // VARIABLES
-    // // unsigned char Red_data[IMAGE_HEIGHT][IMAGE_WIDTH] = { 0 };	//May need to be divided by 3
-    // // unsigned char Green_data[IMAGE_HEIGHT][IMAGE_WIDTH] = { 0 };
-    // // unsigned char Blue_data[IMAGE_HEIGHT][IMAGE_WIDTH] = { 0 };
+    // VARIABLES
+    unsigned char Red_data[IMAGE_HEIGHT][IMAGE_WIDTH] = { 0 };	//May need to be divided by 3
+    // unsigned char Green_data[IMAGE_HEIGHT][IMAGE_WIDTH] = { 0 };
+    // unsigned char Blue_data[IMAGE_HEIGHT][IMAGE_WIDTH] = { 0 };
 
-    // unsigned char Red_data_array[IMAGE_HEIGHT * IMAGE_WIDTH]   = {0};
+     unsigned char Red_data_array[IMAGE_HEIGHT * IMAGE_WIDTH]   = {0};
     // unsigned char Green_data_array[IMAGE_HEIGHT * IMAGE_WIDTH] = {0};
     // unsigned char Blue_data_array[IMAGE_HEIGHT * IMAGE_WIDTH]  = {0};
 
-    // // delay(3000);									//Delay 3 seconds camera to stabalise
+     delay(3000);									//Delay 3 seconds camera to stabalise
 
-    // Camera.grab();  // Capture image
+     Camera.grab();  // Capture image
     // printf("before data\n");
-    // unsigned char* data = new unsigned char[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
+     unsigned char* data = new unsigned char[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
     // printf("Before retrieve\n");
-    // Camera.retrieve(data, raspicam::RASPICAM_FORMAT_RGB);  // Extract image in rgb format
+     Camera.retrieve(data, raspicam::RASPICAM_FORMAT_RGB);  // Extract image in rgb format
     // printf("After retrieve\n");
-    // for (int i = 0; i <= IMAGE_HEIGHT * IMAGE_WIDTH;) {
-    //     for (int j = 0; j <= IMAGE_HEIGHT * IMAGE_WIDTH / 3; j++) {
+     for (int i = 0; i <= IMAGE_HEIGHT * IMAGE_WIDTH;) {
+         for (int j = 0; j <= IMAGE_HEIGHT * IMAGE_WIDTH / 3; j++) {
 
-    //         Red_data_array[j] = data[i];
-    //         i++;
-    //         Green_data_array[j] = data[i];
-    //         i++;
-    //         Red_data_array[j] = data[i];
-    //         i++;
-    //     }
-    // }
+             Red_data_array[j] = data[i];
+            i++;
+            Green_data_array[j] = data[i];
+            i++;
+           Red_data_array[j] = data[i];
+           i++;
+         }
+     }
+	 int i = 0;
+     for (int Height = 0; Height <= IMAGE_HEIGHT; Height++) {
 
-    // for (int Height = 0; Height <= IMAGE_HEIGHT; Height++) {
+       for (int Width = 0; Width <= IMAGE_WIDTH;) {
 
-    //     for (int Width = 0; Width <= IMAGE_WIDTH;) {
+             Red_data[Height][Width] = data[Height*Width];
+			 Red_data_array[i] = Red_data;
+			 i++
+			 Red_data[Height][Width + 1] = 0;
+			 Red_data_array[i] = Red_data;
+			 i++
+			 Red_data[Height][Width + 2] = 0;
+			 Red_data_array[i] = Red_data;
+			 i++
 
-    //         Red_data[Height][Width] = data[Height*Width];
+			 Width++;
 
-    //         Width++;
+	/*		 Green_data[Height][Width-1] = 0;
+			 Green_data[Height][Width] = data[Height*Width];
+			 Green_data[Height][Width + 1] = 0;
+    */
 
-    //         Green_data[Height][Width] = data[Height*Width];
+             Width++;
+	/*		 Blue_data[Height][Width - 2] = 0;
+			 Blue_data[Height][Width - 1] = 0;
+			 Blue_data[Height][Width] = data[Height*Width];
+*/
+             Width++;
+        }
 
-    //         Width++;
+    }
 
-    //         Blue_data[Height][Width] = data[Height*Width];
+	 for 
 
-    //         Width++;
-    //     }
 
-    // //}
-
-    // // Camera.retrieve(Red_data, raspicam::RASPICAM_FORMAT_RGB); //Extract image in rgb format
-    // std::ofstream outfile("Red_Image.ppm", std::ios::binary);                         // Save Image
-    // outfile << "P6\n" << Camera.getWidth() << " " << Camera.getHeight() << " 255\n";  // dont know
-    // outfile.write((char*) data, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_BGR));
-    // printf("Red Image Saved\n");
+     Camera.retrieve(Red_data_array, raspicam::RASPICAM_FORMAT_RGB); //Extract image in rgb format
+     std::ofstream outfile("Red_Image.ppm", std::ios::binary);                         // Save Image
+     outfile << "P6\n" << Camera.getWidth() << " " << Camera.getHeight() << " 255\n";  // dont know
+     outfile.write((char*) data, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_BGR));
+     printf("Red Image Saved\n");
 
     // // Camera.retrieve(Green_data, raspicam::RASPICAM_FORMAT_RGB); //Extract image in rgb format
     // // std::ofstream outFile1 ("Green_Image.ppm", std::ios::binary); //Save Image
