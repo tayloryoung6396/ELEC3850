dcd51f533a5a9d7eb8c5243d0253ff15f0054fa3
ALEXBR <alexrendinaaa@gmail.com>
2018-10-09 13:51:21 +1100

    erir

--- Project/modules/vision/InfraredSensor/src/InfraredSensor.cpp
+++ Project/modules/vision/InfraredSensor/src/InfraredSensor.cpp
@@ -5,11 +5,31 @@
  */
 
 #include "InfraredSensor.hpp"
+// LIBRARIES
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wiringPi.h>
+
+#define IR_TRIG 4
+
+//4 is the IR trig
+//this is 16 on the map
+
 
 void InfraredSensor_init() {
     std::cout << "Initilising INFRARED SENSOR" << std::endl;
+    pinMode(IRTRIG, INPUT);  // IRTRIG IS AN INPUT FROM IR TO RPI
+
 }
 
 int InfraredSensor_main() {
+
+    digitalWrite(IR_TRIG, HIGH);
+    delayMicroseconds(10);  // 10us Delay
+    digitalWrite(IR_TRIG, LOW);
+
+
     return 0;
 }
