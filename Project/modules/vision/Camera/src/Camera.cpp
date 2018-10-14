522a22210db5a0fbc718573f2c8b740cf655ea01
Unknown <c3206230@uon.edu.au>
2018-10-14 22:08:53 +1100

    Fix broken camera

--- Project/modules/vision/Camera/src/Camera.cpp
+++ Project/modules/vision/Camera/src/Camera.cpp
@@ -11,8 +11,6 @@ int Image::Height = 960;
 
 void Camera_init() {
     std::cout << "Initilising CAMERA" << std::endl;
-    //	raspicam::RaspiCam Camera;
-    Camera.set(CV_CAP_PROP_FORMAT, CV_8UC3);
 
     if (!Camera.open()) {  // Error mesage if camera didnt open
 
@@ -29,34 +27,29 @@ void Camera_init() {
 
 int Camera_main() {
 
-    delay(3000);  // Delay 3 seconds camera to stabalise
-
     Camera.grab();
     uint8_t* data           = new uint8_t[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
     uint8_t* Red_data_array = new uint8_t[Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB)];
 
     Camera.retrieve(data, raspicam::RASPICAM_FORMAT_RGB);  // Extract image in rgb format
-                                                           // for (int Height = 0; Height < Image::Height; Height++) {
-    // for (int Width = 0; Width < Image::Width;) {
+
     for (int pixel = 0; pixel < Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB);) {
-        Red_data_array[pixel] = data[pixel]; // Width * Image::Height + Height
+        Red_data_array[pixel] = data[pixel];  // Width * Image::Height + Height
         pixel++;
         Red_data_array[pixel] = 0;  // Width * Image::Height + Height
         pixel++;
         Red_data_array[pixel] = 0;  // Width * Image::Height + Height
         pixel++;
     }
-        //}
-        //}
 
-        printf("Saving file\n");
-        std::ofstream outfile("Red_Image.ppm", std::ios::binary);                         // Save Image
-        outfile << "P6\n" << Camera.getWidth() << " " << Camera.getHeight() << " 255\n";  // dont know
-        outfile.write((char*) Red_data_array, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
-        printf("Red Image Saved\n");
+    printf("Saving file\n");
+    std::ofstream outfile("Red_Image.ppm", std::ios::binary);                         // Save Image
+    outfile << "P6\n" << Camera.getWidth() << " " << Camera.getHeight() << " 255\n";  // dont know
+    outfile.write((char*) Red_data_array, Camera.getImageTypeSize(raspicam::RASPICAM_FORMAT_RGB));
+    printf("Red Image Saved\n");
 
-        delete Red_data_array;
-        delete data;
+    delete Red_data_array;
+    delete data;
 
-        return 0;
-    }
+    return 0;
+}
