06ad95bd650cea433fbaf89d4c6ee8ffa803c6db
ALEXBR <alexrendinaaa@gmail.com>
2018-11-05 13:49:35 +1100

    LED

---
+++ Project/modules/vision/LedStatus/src/LedStatus.cpp
@@ -0,0 +1,27 @@
+#include "LedStatus.hpp"
+//17 27 22
+#define LED_RED 17
+#define LED_BLUE 22
+#define LED_GREEN 27
+
+void Enable_RED(){
+    digitalWrite(LED_RED, HIGH);
+}
+
+void Disable_RED(){
+    digitalWrite(LED_RED, LOW);
+}
+void Enable_BLUE
+    digitalWrite(LED_BLUE, HIGH);
+}
+
+void Disable_BLUE(){
+    digitalWrite(LED_BLUE, LOW);
+}
+void Enable_GREEN(){
+    digitalWrite(LED_GREEN, HIGH);
+}
+
+void Disable_GREEN(){
+    digitalWrite(LED_GREEN, LOW);
+}
