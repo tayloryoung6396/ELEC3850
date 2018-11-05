013e1c85654b4984676091502a2f9c97fb67e6c7
ALEXBR <alexrendinaaa@gmail.com>
2018-11-05 13:32:08 +1100

    ir yes

--- Project/modules/vision/InfraredSensor/src/InfraredSensor.cpp
+++ Project/modules/vision/InfraredSensor/src/InfraredSensor.cpp
@@ -12,16 +12,21 @@
 #include <string.h>
 #include <wiringPi.h>
 
-#define IR_TRIG 4
+#define IRTRIG 4
 
 //4 is the IR trig
 //this is 16 on the map
-
 
 void InfraredSensor_init() {
     std::cout << "Initilising INFRARED SENSOR" << std::endl;
 <<<<<<< HEAD
     pinMode(IRTRIG, INPUT);  // IRTRIG IS AN INPUT FROM IR TO RPI
+
+
+    digitalRead(IR_TRIG);
+    delayMicroseconds(60);  // 60us Delay
+
+
 =======
 >>>>>>> test_branch_alex
 
@@ -29,10 +34,39 @@ void InfraredSensor_init() {
 
 int InfraredSensor_main() {
 
-    digitalWrite(IR_TRIG, HIGH);
-    delayMicroseconds(10);  // 10us Delay
-    digitalWrite(IR_TRIG, LOW);
-
+    IRdetect();
+    if (IRsense::meanIR>=2)
+        IRsense::IRpresent= 1;
+    else IRsense::IRpresent=0;
 
     return 0;
 }
+
+
+void IRdetect() {
+    // Reset all of the times
+    IRsense::IR_sensor_return[0] =-1;
+    IRsense::IR_sensor_return[1] =-1;
+    IRsense::IR_sensor_return[2] =-1;
+    IRsense::IR_sensor_return[3] =-1;
+    IRsense::meanIR=0;
+
+        for(int sensor = 0; sensor < IRsense::IR_attempts; sensor++)
+        {
+            IRsense::IR_sensor_return[sensor]=digitalRead(IR_TRIG);
+
+            if (IRsense::IR_sensor_return[i]=-1)
+            {
+                std::cout << "Error w/ IR" ; }
+            else
+                IRsense::meanIR+=IRsense::IR_sensor_return[sensor];
+        }
+
+    IRsense::meanIR=IRsense::meanIR/IRsense::IR_attempts;   //there are 4 tests, if majority are true (1) then probs have an IR present.
+
+    delayMicroseconds(10);          //chuck in a delay for good measure am i right
+
+    }
+
+
+
--- Project/modules/vision/InfraredSensor/src/InfraredSensor.hpp
+++ Project/modules/vision/InfraredSensor/src/InfraredSensor.hpp
@@ -6,4 +6,12 @@
 extern void InfraredSensor_init();
 extern int InfraredSensor_main();
 
+
+class IRsense {
+public:
+    int IR_attempts=3;
+    int IR_sensor_return[IR_attempts];
+    double meanIR;
+    bool IRpresent;
+};
 #endif /* INFRARED_SENSOR_H_ */
