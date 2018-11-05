a3b53854af05b2c22e36eefe76daccdf2fc90138
Unknown <genevieve-24@hotmail.com>
2018-11-05 14:50:14 +1100

    Classifier condensed - hasn't been checked

--- Project/modules/vision/Classifier/src/Classifier.cpp
+++ Project/modules/vision/Classifier/src/Classifier.cpp
@@ -122,3 +122,146 @@ int Classifier_main() {
     // // Something
     return 0;
 }
+
+// ClassificationT2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#define WIDTH_MIN 100
+#define WIDTH_MAX 200
+#define HEIGHT_MIN 100
+#define HEIGHT_MAX 200
+
+
+#define IMAGE_HEIGHT 1280
+#define IMAGE_WIDTH 960
+
+int Image_Size = IMAGE_HEIGHT * IMAGE_WIDTH * 3;
+//colours[How many colours][R,G,B][Min,Max]
+int colours[3][3][2] = { { { 64,97 },{ 19,48 },{ 19,47 } },		//Red maximum and minimum pixel parameters (RGB Image)
+{ { 1,9 },{ 11,49 },{ 47,61 } },		//Green maximum and minimum pixel parameters (RGB Image)
+{ { 14,33 },{ 44,61 },{ 79,111 } },		//Blue maximum and minimum pixel parameters (RGB Image)
+};
+
+//int seed[][] = { {colour, pixel}, {} }
+int seed[][] = ;
+
+//int object[][] = {{center_x, center_y, width, height}, {} }
+int object[][] = ;
+
+void Classifier(uint8_t* data)
+{
+	int pixel = 0;		//start at the first pixel of the image 
+	int colour = 0;
+	int i = 0;
+	int k = 0;
+
+	for (pixel = 0; pixel <= Image_Size; pixel += 3) {
+
+		for (colour = 0; colour <= 3; colour++) {
+
+			if (pixel >= colours[colour][0][0] && pixel <= colours[colour][0][1]
+				&& (pixel + 1) >= colours[colour][1][0] && (pixel + 1) <= colours[colour][1][1]
+				&& (pixel + 2) >= colours[colour][2][0] && (pixel + 1) <= colours[colour][2][1]
+				)
+			{
+				seed[i][0] = colour;
+				seed[i][1] = pixel;
+				i++;
+
+				break;
+			}
+
+		}
+
+	}
+
+	//Insert some for loop to cancel out seed points included in previously found objects 
+
+	//cycle through seed points
+	for (j = 0; j <= i; j++) {
+
+		int left = 0;
+		int right = 0;
+		int up = 0;
+		int down = 0;
+
+		colour = seed[j][0];
+		pixel = seed[j][1];
+
+		R_min = colours[colour][0][0];
+		R_max = colours[colour][0][1];
+		G_min = colours[colour][1][0];
+		G_max = colours[colour][1][1];
+		B_min = colours[colour][2][0];
+		B_max = colours[colour][2][1];
+
+
+		//cluster right
+		while (R_min <= data[pixel] && R_max >= data[pixel]
+			&& G_min <= data[(pixel + 1)] && G_max >= data[(pixel + 1)]
+			&& B_min <= data[(pixel + 2)] && B_max >= data[(pixel + 2)]
+			) {
+
+			right++;
+			pixel += 3;
+		}
+
+		//cluster left
+		pixel = seed[j][1];
+		while (R_min <= data[(pixel - 3)] && R_max >= data[(pixel - 3)]
+			&& G_min <= data[(pixel - 2)] && G_max >= data[(pixel - 2)]
+			&& B_min <= data[(pixel - 1)] && B_max >= data[(pixel - 1)]
+			) {
+
+			left++;
+			pixel = pixel - 3;
+		}
+
+		//cluster up
+		pixel = seed[j][1];
+		while (R_min <= data[pixel] && R_max >= data[pixel]
+			&& G_min <= data[(pixel + 1)] && G_max >= data[(pixel + 1)]
+			&& B_min <= data[(pixel + 2)] && B_max >= data[(pixel + 2)]
+			) {
+
+			up++;
+			pixel = pixel - IMAGE_WIDTH / 3;
+		}
+
+		//cluster down
+		pixel = seed[j][1];
+		while (R_min <= data[pixel] && R_max >= data[pixel]
+			&& G_min <= data[(pixel + 1)] && G_max >= data[(pixel + 1)]
+			&& B_min <= data[(pixel + 2)] && B_max >= data[(pixel + 2)]
+			) {
+
+			down++;
+			pixel = pixel + IMAGE_WIDTH / 3;
+		}
+
+		//calulate object width and height from clustering
+		int width = left + right;
+		int height = up + down;
+
+		//Check object size to target object parameters
+
+		if (width >= WIDTH_MIN && width <= WIDTH_MAX && height >= HEIGHT_MIN && height <= HEIGHT_MAX) {
+
+			//Calculate centers
+			pixel = seed[j][1];
+			int center_x = ((pixel - left) + (pixel + right)) / 2;
+			int center_y = ((pixel - up * (IMAGE_WIDTH / 3)) + (pixel + down * (IMAGE_WIDTH / 3))) / 2;
+
+			object[k][0] = center_x;
+			object[k][1] = center_y;
+			object[k][2] = width;
+			object[k][3] = height;
+
+			k++;
+		}
+	}
+
+	return;
+}
+
