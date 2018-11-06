aaf591e718d680fbc881ff846798c4bee029fb56
Unknown <c3206230@uon.edu.au>
2018-11-07 08:17:57 +1100

    Update tester

--- Project/modules/vision/Classifier/test/Classifier_tester.hpp
+++ Project/modules/vision/Classifier/test/Classifier_tester.hpp
@@ -4,39 +4,39 @@
 // #include "main.hpp"
 
 // static raspicam::RaspiCam Camera;  // Open camera
+#include <errno.h>
+#include <malloc.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <cfloat>
 #include <cmath>
 #include <csignal>
 #include <cstdio>
 #include <ctime>
-#include <errno.h>
 #include <fstream>
 #include <iostream>
 #include <limits>
-#include <malloc.h>
-#include <math.h>
 #include <set>
 #include <stack>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <string>
 #include <vector>
 
 class Image {
 public:
-  static int Height;
-  static int Width;
+    static int Height;
+    static int Width;
 };
 
 class Camera {
 public:
-  static const double pixel_x;
-  static const double pixel_y;
-  static const double resolution_x;
-  static const double resolution_y;
-  static const double focal_len;
+    static const double pixel_x;
+    static const double pixel_y;
+    static const double resolution_x;
+    static const double resolution_y;
+    static const double focal_len;
 };
 
 // #endif /* CAMERA_H_ */
